2b8a13ba87f15768652c16e01ced30c6a3d24f4d
下村 翔 <shimo@agusa.i.is.nagoya-u.ac.jp>
2009-05-13 16:16:08 +0900

    add predicate

---
+++ Predicate.h
@@ -0,0 +1,22 @@
+//
+//  Predicate.h
+//  TODO
+//
+//  Created by 下村 翔 on 5/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface Predicate :  NSManagedObject  
+{
+}
+
+@property (retain) NSString * title;
+@property (retain) NSNumber * isSmartList;
+@property (retain) NSString * predicate;
+
+@end
+
+
---
+++ Predicate.m
@@ -0,0 +1,18 @@
+// 
+//  Predicate.m
+//  TODO
+//
+//  Created by 下村 翔 on 5/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Predicate.h"
+
+
+@implementation Predicate 
+
+@dynamic title;
+@dynamic isSmartList;
+@dynamic predicate;
+
+@end
